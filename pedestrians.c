492b143fcbaab2cde7d5bf8e1e455710e8abaaa1
Mati Mutz <matias.mutz@gmail.com>
2022-07-06 16:08:47 -0300

    avance en el main

--- pedestrians.c
+++ pedestrians.c
@@ -4,13 +4,26 @@
 
 #define ARGS 3 //debe ser el argumento del ejecutable mas los dos nombres de los archivos
 
-enum ERRORES {OK=0, ARG_INV, NO_MEMORY};
+enum ERRORS {OK=0, ARG_INV, NOT_EXIST ,NO_MEMORY = ENOMEM};
 
 int main(int argc, char *argv[]){
     if( argc != ARGS ) {
-        printf("La cantidad de argumentos ingresada no es valida");
+        printf("La cantidad de argumentos ingresada no es valida.\n");
         return ARG_INV;
     }
-    //comenzamos con el codigo
-    return 0;
+    dataADT data;
+    enum ERRORS processResult = processData(argv[1], argv[2], &data);
+    if(processResult == NOT_EXIST) {
+        printf("El archivo pasado por parametro no existe o no fue encontrado.\n");
+        return NO_MEMORY;
+    }
+    else if(processResult == NO_MEMORY){
+        printf("No hay espacio disponible para guardar datos.\n");
+        return NO_MEMORY;
+    }
+    query1(data);
+    query2(data);
+    query3(data);
+    freeAll(data);
+    return OK;
 }
