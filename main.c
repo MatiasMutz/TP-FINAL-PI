77584bbbfc26a7c2c5ca63690ec780d621600636
Mati Mutz <matias.mutz@gmail.com>
2022-07-06 01:33:05 -0300

    Agrego main

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARGS 3
+
+int main(int argc, char *argv[]){
+    if( argc != ARGS ) {
+        printf("La cantidad de argumentos ingresada no es valida");
+        //dar un error
+    }
+    //comenzamos con el codigo
+}
