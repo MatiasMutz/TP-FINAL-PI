5f6bf96c8cc3bd65f6a8e2b7cf3db89e0fb1c0f1
Boullosa <108482692+boullo2002@users.noreply.github.com>
2022-07-06 14:01:42 -0300

    agrugue las structs

--- dataADT.c
+++ dataADT.c
@@ -1,2 +1,24 @@
  #include "dataADT.h"
- 
+
+typedef struct elemQ1{
+    size_t id;
+    size_t cantP_sensor;
+    char* name;
+    struct elemQ1* tail;
+}elemQ1;
+
+typedef struct elemQ2{
+    unsigned int anio;
+    size_t cantP_anio;
+}elemQ2;
+
+typedef struct elemQ3{
+    char* dia;
+    size_t cantP_diurno;
+    size_t cantP_nocturno;
+}elemQ3;
+ typedef struct dataCDT{
+    elemQ1* firstQ1;
+    elemQ2* firstQ2;
+    elemQ3 dias[7];
+ }dataCDT;
