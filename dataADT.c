03d335d735a8a76128507fc8b0b81bd72f880a31
Boullosa <108482692+boullo2002@users.noreply.github.com>
2022-07-06 13:47:03 -0300

    agregue los include

--- dataADT.c
+++ dataADT.c
@@ -1,1 +1,2 @@
+ #include "dataADT.h"
  
--- dataADT.h
+++ dataADT.h
@@ -1,4 +1,7 @@
 #ifndef TPE_FINAL_DATAADT_H
 #define TPE_FINAL_DATAADT_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #endif //TPE_FINAL_DATAADT_H
