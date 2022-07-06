a360e3b6e037dd18062edaf18c9a4f6e104d2b21
Mati Mutz <matias.mutz@gmail.com>
2022-07-06 15:08:24 -0300

    Cambio nombre main

--- pedestrians.c
+++ pedestrians.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "dataADT.h"
 
-#define ARGS 3
+#define ARGS 3 //debe ser el argumento del ejecutable mas los dos nombres de los archivos
+
+enum ERRORES {OK=0, ARG_INV, NO_MEMORY};
 
 int main(int argc, char *argv[]){
     if( argc != ARGS ) {
         printf("La cantidad de argumentos ingresada no es valida");
-        //dar un error
+        return ARG_INV;
     }
     //comenzamos con el codigo
+    return 0;
 }
