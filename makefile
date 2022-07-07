fa543688715eb5c3787242195121807ce0add66a
Mati Mutz <matias.mutz@gmail.com>
2022-07-07 16:53:41 -0300

    .

--- makefile
+++ makefile
@@ -1,8 +1,10 @@
 COMPILER = gcc
+
 DEBUG_COMPILER = -g -DDEBUG=1
-OUTPUT_FILE = pedestrians.out
-FILES = pedestrians.c dataADT.c lectura.c querys.c
-FLAGS = -g -std=c99 -pedantic -Wall -fsanitize=address
+
+OUTPUT_FILE = pedestrians
+FILES = pedestrians.c dataADT.c lectura.c queries.c
+FLAGS = -std=c99 -pedantic -Wall -fsanitize=address
 
 all:
 	$(COMPILER) -o $(OUTPUT_FILE) $(FILES) $(FLAGS)
--- pedestrians.c
+++ pedestrians.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dataADT.h"
-#include"lectura.h"
+#include "lectura.h"
 #define MAX_LINE 1024
 #define VERIFICA_PROCESADO if(qResult == NOT_PROCESSED) {\
                                 printf("Los datos no fueron procesados.\n");\
@@ -28,10 +28,10 @@ int main(int argc, char *argv[]){
         return ENOMEM;
     }
     //ABRO AMBOS ARCHIVOS
-    FILE *sensors = fopen(sensor, "rt");
+    FILE* sensors = fopen(sensor, "rt");
     if(sensors == NULL)
         return NOT_EXIST;
-    FILE *readings = fopen(reading, "rt");
+    FILE* readings = fopen(reading, "rt");
     if(readings == NULL){
         fclose(sensors);
         return NOT_EXIST;
@@ -40,6 +40,7 @@ int main(int argc, char *argv[]){
     leerSensors(&id, &name, &activo, sensors, line);
     leerReadings(&year, &time, &id, &day, &people, readings, line);
 
+    /*
     enum ERRORS processResult = processData(argv[1], argv[2], &data);
     if(processResult == NOT_EXIST) {
         printf("El archivo pasado por parametro no existe o no fue encontrado.\n");
@@ -49,10 +50,7 @@ int main(int argc, char *argv[]){
         printf("No hay espacio disponible para guardar datos.\n");
         return NO_MEMORY;
     }
-
-
-
-
+     */
     //SALIDA
     enum ERRORS qResult;
 
