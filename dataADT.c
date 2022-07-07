e5e2ef3e51b4a49eb4ffe616d79b3ec7f5eb385b
bendeyhe <108757682+bendeyhe@users.noreply.github.com>
2022-07-07 00:04:18 -0300

    cambio minimo

--- dataADT.c
+++ dataADT.c
@@ -256,7 +256,6 @@ static int hasNext(const listQ2 l){
 }
 
 static listQ2 next(listQ2 l){
-     assert(hasNext(l)); //chequeo que no tendriamos que hacer en el backend, corregir
      listQ2 aux = l->iterador;
      l->iterador = l->iterador->tail;
      return aux;
