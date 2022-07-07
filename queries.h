e9a8a80287cb84743b803e14cf4abbe92702ab72
Boullosa <108482692+boullo2002@users.noreply.github.com>
2022-07-07 15:14:35 -0300

    .

--- queries.h
+++ queries.h
@@ -1,12 +1,26 @@
 #ifndef TPE_FINAL_QUERIES_H
 #define TPE_FINAL_QUERIES_H
 #include "dataADT.h"
-//Carga los datos del query 1 en el archivo csv
+
+/*
+ * deja en un csv los resultados de la query1.
+ * devuelve OK (0) si no hubo errores y se genero el csv correctamente.
+ * devuelve NOT_PROCESSED si se la llama sin procesar los datos.
+ */
 int query1(dataADT data);
 
+/*
+ * deja en un csv los resultados de la query2.
+ * devuelve OK (0) si no hubo errores y se genero el csv correctamente.
+ * devuelve NOT_PROCESSED si se la llama sin procesar los datos.
+ */
 int query2(dataADT data);
 
-//Carga los datos del query 3 en el archivo csv
+/*
+ * deja en un csv los resultados de la query3.
+ * devuelve OK (0) si no hubo errores y se genero el csv correctamente.
+ * devuelve NOT_PROCESSED si se la llama sin procesar los datos.
+ */
 int query3(dataADT data);
 
 #endif
